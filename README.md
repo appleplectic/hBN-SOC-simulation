baee4c574c97a8b4a08fae6729994a9a5a91d9bb
Levin Ma <llmar1000000@gmail.com>
2025-07-13 21:59:09 +0000

    Updated excited state configuration

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# h-BN Simulation
+# h-BN (boron vacancy) Simulation
+Determining the effects of spin-orbit coupling (SOC) on quantum emission simulations in monolayer hBN with a boron vacancy. 
--- run.sh
+++ run.sh
@@ -1,15 +1,26 @@
-python generate_structure.py  # outputs hbn_vb.atoms; need to copy paste output
+echo "Generating hbn_vb.atoms..."
+python generate_structure.py
+
+echo "Generated; copy output into *.in files. Press enter to continue..."
+read
 
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 
-screen -S qe-run bash -c "\ 
-rm -rf scratch ; \ 
-rm out/* ; \ 
-mpirun -np 1 --bind-to none pw.x -in pw_scf_SOC.in > out/SOC.out ; \ 
-mpirun -np 1 --bind-to none pw.x -in pw_scf_noSOC.in > out/noSOC.out ; \ 
-cp -r ./scratch/hbn_vb_nosoc.save ./scratch/hbn_vb_excited_nosoc.save ; \ 
-cp -r ./scratch/hbn_vb_soc.save ./scratch/hbn_vb_excited_soc.save ; \ 
-mpirun -np 1 --bind-to none pw.x -in pw_scf_excited_noSOC.in > out/excited_noSOC.out ; \ 
-mpirun -np 1 --bind-to none pw.x -in pw_scf_excited_SOC.in > out/excited_SOC.out ; \ 
-"
+rm -rf scratch
+rm out/*
+
+echo "Running ground state no SOC calculations..."
+mpirun -np 1 --bind-to none pw.x -in pw_scf_noSOC.in > out/noSOC.out
+
+echo "Running ground state SOC calculations..."
+cp -r scratch/hbn_vb_nosoc.save scratch/hbn_vb_soc.save
+mpirun -np 1 --bind-to none pw.x -in pw_scf_SOC.in > out/SOC.out
+
+echo "Running excited state no SOC calculations..."
+cp -r scratch/hbn_vb_nosoc.save scratch/hbn_vb_excited_nosoc.save
+mpirun -np 1 --bind-to none pw.x -in pw_scf_excited_noSOC.in > out/excited_noSOC.out 
+
+echo "Running excited state SOC calculations..."
+cp -r scratch/hbn_vb_excited_nosoc.save scratch/hbn_vb_excited_soc.save
+mpirun -np 1 --bind-to none pw.x -in pw_scf_excited_SOC.in > out/excited_SOC.out 
